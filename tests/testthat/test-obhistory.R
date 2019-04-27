d296812746ef78fd19ca7951c88646d0a75612eb
floatgreen <yikelikeli@gmail.com>
2019-04-27 01:20:30 -0500

    update

--- tests/testthat/test-obhistory.R
+++ tests/testthat/test-obhistory.R
@@ -3,11 +3,11 @@ context("test-obhistory")
 test_that("correct input", {
   expect_error(obhistory(2333), "id is not a string")
   expect_error(obhistory("KAADF"),"id is not 4 characters")
-  expect_error(obhistory("AAAA"),"not a correct ID")
+  expect_error(obhistory("AAAA"),"url is not valid, maybe not a correct ID")
   expect_error(obhistory(c("KORD", "KAVX")),"num of argument should be 1")
   expect_error(obhistory(), "num of argument should be 1")
 })
 
 test_that("return proper dataframe", {
-  expect_equal(ncol(obhistory("KORD")),7)
+  expect_equal(ncol(obhistory("KORD")),9)
 })
--- tests/testthat/test-plot_temp_history.R
+++ tests/testthat/test-plot_temp_history.R
@@ -4,7 +4,7 @@ context("test-plot_temp_history")
 test_that("correct input", {
   expect_error(plot_temp_history(2333), "id is not a string")
   expect_error(plot_temp_history("KAADF"),"id is not 4 characters")
-  expect_error(plot_temp_history("AAAA"),"not a correct ID")
+  expect_error(plot_temp_history("AAAA"),"url is not valid, maybe not a correct ID")
   expect_error(plot_temp_history(c("KORD", "KAVX")),"num of argument should be 1")
   expect_error(plot_temp_history(), "num of argument should be 1")
 })
